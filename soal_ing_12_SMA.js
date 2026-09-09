02eb5bec5e83c9cc354e9757373ac6e9542cd8e0
AKTM <andrea.yk44@guru.smp.belajar.id>
2026-09-09 11:02:16 +0700

    Update soal_ing_12_SMA.js

--- soal_ing_12_SMA.js
+++ soal_ing_12_SMA.js
@@ -4,7 +4,7 @@ window.soalING = [
     // ========================================
     {
         id: 31,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "Sebuah kandung ayam dapat menampung 1.500 ekor ayam. Seorang peternak mempunyai 1.025 ekor ayam, setelah menambah sebanyak 98 ekor ayam baru. Untuk memaksimalkan kapasitas kandang ayam, maka peternak harus membeli ... ekor ayam lagi.",
@@ -23,7 +23,7 @@ window.soalING = [
     // ========================================
     {
         id: 32,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "120% - 3 + 2 x 0,75 + 2/3 = ...",
@@ -42,7 +42,7 @@ window.soalING = [
     // ========================================
     {
         id: 33,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "Pak Bayu akan membeli beras keperluan sebesar di toko \"Amanah\". Pak Bayu membeli dua uang seratus ribuan dan satu lembar uang lima puluh ribuan. Pak Bayu membeli 5 kg beras, 2 kg gula pasir, dan 2 liter minyak goreng.<br><br><b>Harga Barang Toko \"Amanah\"</b><br>Beras Rp 15.000,00/Kg<br>Gula pasir Rp 11.000,00/Kg<br>Minyak goreng Rp 12.000,00/Liter<br><br>Setiap pembelian Rp 100.000,00 dapat potongan harga Rp 10.000,00.<br><br>Pernyataan berikut yang benar adalah... (Pilih lebih dari satu jawaban benar)",
@@ -61,7 +61,7 @@ window.soalING = [
     // ========================================
     {
         id: 34,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "BS",
         soal: "<img src=\"images_tka_6/34.png\" alt=\"Gambar Soal 34\" style=\"max-width:200px; display:block; margin:10px auto;\"><br><b>Pernyataan yang sesuai dengan gambar berwarna tersebut!</b><br><br>(1) Pecahan yang sesuai dengan gambar adalah 3/9.<br>(2) Pecahan senilai yang sesuai adalah 1/4.<br>(3) Pecahan senilai yang sesuai adalah 5/15.",
@@ -79,7 +79,7 @@ window.soalING = [
 // ========================================
 {
     id: 35,
-    kategori: "MATEMATIKA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Dalam pembelajaran pecahan Bu Siska membagikan kartu pecahan kepada Abi, Fatih, Genta, Kesya, dan Ines. Ines mendapatkan kartu bertuliskan 22/6, Abi mendapatkan kartu 365%, Fatih mendapatkan kartu 3 11/16, sedangkan Genta 3,61 dan Kesya mendapatkan kartu 40/11. Kelima anak tersebut diminta berdiri di depan kelas.<br><br>Urutan nama anak dari yang terkecil berturut-turut adalah ...",
@@ -98,7 +98,7 @@ window.soalING = [
     // ========================================
     {
         id: 36,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "Paman mempunyai 3 lahan kebun. Pada musim panen, lahan kebun pertama menghasilkan 3/4 kuintal buah jeruk, lahan kebun kedua menghasilkan 2/5 kuintal buah apel, dan lahan kebun ketiga menghasilkan 3/10 kuintal buah mangga.<br><br>(1) Jumlah panen buah jeruk dan buah apel ada 1 1/10 kuintal<br>(2) Jumlah panen buah jeruk dan buah mangga ada 1 1/20 kuintal<br>(3) Selisih panen buah apel dan buah mangga ada 1/10 kuintal<br>(4) Selisih panen buah jeruk dan mangga ada 9/20 kuintal<br><br>Pernyataan yang benar adalah ...",
@@ -117,7 +117,7 @@ window.soalING = [
     // ========================================
     {
         id: 37,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "Bu Wati membeli 6 botol minyak goreng. Setiap botol berisi 1/2 liter minyak goreng. Sisa minyak goreng di dalam wadah plastik berukuran 3/4 liter. Banyak wadah plastik yang dibutuhkan Bu Wati untuk menampung semua minyak goreng adalah ... buah",
@@ -136,7 +136,7 @@ window.soalING = [
     // ========================================
     {
         id: 38,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "Jumlah kelereng Agus 2/3 kali jumlah kelereng Budi. Jika jumlah kelereng mereka 20 butir, maka ...<br><br>(1) Banyak kelereng Agus 8 butir<br>(2) Banyak kelereng Budi 10 butir<br>(3) Selisih kelereng mereka 4 butir<br>(4) Jika Budi membeli 3 butir kelereng lagi, maka jumlah kelereng sekarang ada 15 butir",
@@ -155,7 +155,7 @@ window.soalING = [
     // ========================================
     {
         id: 39,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "BS",
         soal: "Kakak akan menyambungkan beberapa pita. Sisa pita kebenaran sama rata. Jika pita yang tersambung 72 bungkus misal per instan, 60 bungkus sisa, dan 48 bungkus sisa. Pernyataan Benar atau Salah yang tepat adalah...<br><br>(1) Banyak pita sekadar 12<br>(2) Total item dalam setiap pita sama sisa 15<br>(3) Banyak mie instan sama setiap pita sekadar ada 5",
@@ -173,7 +173,7 @@ window.soalING = [
     // ========================================
     {
         id: 40,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "<b>Perhatikan!</b><br><br>Les renang \"JUARA\"<br>Tersedia:<br>• Paket 1: les setiap 2 hari sekali, dimulai 2 September 2025<br>• Paket 2: les setiap 3 hari sekali, dimulai 5 September 2025<br>• Paket 3: les setiap 5 hari sekali, dimulai 9 September 2025<br><br>Jani mengambil paket 1, Lila mengambil paket 2, dan Fira mengambil paket 3.<br><br>(1) Mereka akan les bersama pertama kali tanggal 20 September 2025.<br>(2) Mereka akan les bersama pertama kali tanggal 14 September 2025.<br>(3) Pada bulan September 2025, Fira akan les sebanyak 5 kali.<br>(4) Mereka akan les bersama setiap 20 hari sekali.<br><br>Pernyataan yang benar adalah ... (jawaban benar lebih dari satu)",
@@ -192,7 +192,7 @@ window.soalING = [
     // ========================================
     {
         id: 41,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "BS",
         soal: "Berikut beberapa sifat bangun datar persegi panjang:<br><br>(1) Memiliki 4 sisi yang sama panjang.<br>(2) Mempunyai 2 pasang sisi yang sama panjang.<br>(3) Keempat sudutnya sama besar (90°).",
@@ -210,7 +210,7 @@ window.soalING = [
     // ========================================
     {
         id: 42,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "Tiko akan membentuk mainan dadu dari karton seperti gambar di atas. Jumlah mata dadu yang saling berhadapan jumlahnya sama yaitu 7. Pola jaring-jaring mainan dari bahan Tiko yang tepat adalah ...",
@@ -229,7 +229,7 @@ window.soalING = [
     // ========================================
     {
         id: 43,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "Jika jarak rumah Andi ke sekolah 800 m dan jarak rumah Budi ke sekolah 1.200 m, maka ...<br><br>(1) Jarak rumah Andi dan Budi 2 km<br>(2) Jarak rumah Andi ke sekolah 8 km<br>(3) Jarak rumah Budi ke sekolah 12 km<br>(4) Selisih jarak rumah mereka ke sekolah 400 m<br><br>Pernyataan yang benar adalah ... (jawaban benar lebih dari satu)",
@@ -248,7 +248,7 @@ window.soalING = [
     // ========================================
     {
         id: 44,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "Seorang pedagang dengan mobil pick up mengangkut berbagai macam barang, kedelai, beras, dan jagung. Berat kedelai 0,24 ton, beras 326 kg, dan jagung 3,08 kuintal. Di pasar pedagang tersebut menurunkan beras seberat 1/4 kuintal dan kedelai 125 kg. Sisa beratnya dibawa ke Gudang. Berat muatan yang dibawa ke gudang adalah ... kg",
@@ -267,7 +267,7 @@ window.soalING = [
     // ========================================
     {
         id: 45,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "<img src=\"images_tka_6/45.png\" alt=\"Gambar Soal 45\" style=\"max-width:220px; display:block; margin:10px auto;\"><br>Besar sudut WZY pada gambar tersebut adalah ...",
@@ -286,7 +286,7 @@ window.soalING = [
     // ========================================
     {
         id: 46,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "<img src=\"images_tka_6/46.png\" alt=\"Gambar Soal 46\" style=\"max-width:260px; display:block; margin:10px auto;\"><br>Sebuah pigura foto dibuat Lina dari kertas karton. Pigura tersebut berbentuk seperti pada gambar. Bagian tengah untuk pemasangan foto. Bagian lain dilapisi kertas warna. Luas minimal kertas warna yang diperlukan adalah ...",
@@ -305,7 +305,7 @@ window.soalING = [
     // ========================================
     {
         id: 47,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PG",
         soal: "Sebuah bak mandi berisi penuh air 80.000 cl. Jika 3/4 bagian sudah digunakan untuk mandi, maka sisa air di dalam bak mandi ... l",
@@ -324,7 +324,7 @@ window.soalING = [
     // ========================================
     {
         id: 48,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "BS",
         soal: "Ibu membeli beras 5 kg, gula pasir 3.500 gram, dan telur 20 ons.<br><br>Tentukan Benar atau Salah pada setiap pernyataan berikut!",
@@ -342,7 +342,7 @@ window.soalING = [
     // ========================================
     {
         id: 49,
-        kategori: "MATEMATIKA",
+        kategori: "BAHASA INGGRIS",
         rentangNilai: "0-3",
         tipe: "PGK",
         soal: "<img src=\"images_tka_6/49.png\" alt=\"Gambar Soal 49\" style=\"max-width:220px; display:block; margin:10px auto;\"><br>Bak tersebut berisi air seperempat bagian (π = 22/7). Dari gambar tersebut dapat disimpulkan pernyataan berikut:<br><br>(1) Ketinggian air dalam bak adalah 50 dm.<br>(2) Ketinggian air dalam bak adalah 12,5 dm.<br>(3) Volume air dalam bak penampungan tersebut adalah 7.700 dm³.<br>(4) Volume air dalam bak penampungan tersebut adalah 30.800 dm³.<br><br>Pernyataan yang benar ditunjukkan nomor ...",
@@ -364,7 +364,7 @@ window.soalING = [
         kategori: "NUMERASI",
         rentangNilai: "0-3",
         tipe: "PG",
-        soal: "Hasil penilaian harian IPA siswa kelas VI SD Merdeka sebagai berikut:<br><br>75, 83, 89, 80, 83, 85, 80, 83, 80, 89,<br>83, 85, 95, 83, 75, 89, 80, 89, 85, 83,<br>85, 83, 89, 83, 95, 75, 85, dan 83.<br><br>Tabel yang sesuai data tersebut adalah ...",
+        soal: "Hasil penilaian harian BAHASA INGGRIS siswa kelas VI SD Merdeka sebagai berikut:<br><br>75, 83, 89, 80, 83, 85, 80, 83, 80, 89,<br>83, 85, 95, 83, 75, 89, 80, 89, 85, 83,<br>85, 83, 89, 83, 95, 75, 85, dan 83.<br><br>Tabel yang sesuai data tersebut adalah ...",
         gambar: null,
         pilihan: [
             "A.<br><table border='1' style='border-collapse: collapse; text-align: center; width: 100%; margin-top: 4px;'><tr><td style='padding: 4px;'>Nilai</td><td style='padding: 4px;'>75</td><td style='padding: 4px;'>80</td><td style='padding: 4px;'>83</td><td style='padding: 4px;'>85</td><td style='padding: 4px;'>89</td><td style='padding: 4px;'>95</td></tr><tr><td style='padding: 4px;'>Jumlah siswa</td><td style='padding: 4px;'>3</td><td style='padding: 4px;'>4</td><td style='padding: 4px;'>9</td><td style='padding: 4px;'>5</td><td style='padding: 4px;'>5</td><td style='padding: 4px;'>2</td></tr></table>",
@@ -388,8 +388,8 @@ window.soalING = [
     <div style="text-align:center; padding:30px 20px; background-color:#fff9c4; border:2px solid #fbc02d; border-radius:12px; color:#856404;">
       <h2 style="margin:0 0 15px 0; font-size:22px; font-weight:900;">📢 PERHATIAN</h2>
       <p style="font-size:17px; line-height:1.6; margin:0;">
-        Anda telah menyelesaikan bagian <strong>TKA MATEMATIKA</strong>.<br><br>
-        Selanjutnya Anda akan mengerjakan soal bagian <strong>TKA ILMU PENGETAHUAN ALAM (IPA)</strong>.<br>
+        Anda telah menyelesaikan bagian <strong>TKA BAHASA INGGRIS</strong>.<br><br>
+        Selanjutnya Anda akan mengerjakan soal bagian <strong>TKA ILMU PENGETAHUAN ALAM (BAHASA INGGRIS)</strong>.<br>
         Silakan tekan tombol <strong>SELANJUTNYA</strong> di bawah ini untuk melanjutkan ujian.
       </p>
     </div>
@@ -404,7 +404,7 @@ window.soalING = [
 // ========================================
 {
     id: 51,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Bakteri E-coli di usus besar membantu mengurangi sisa makanan. Sementara di situ bakteri mendapatkan tempat untuk hidup. Coba perhatikan gambar berikut!<br><br>Gambar yang memiliki kesamaan hubungan dengan bacaan di atas adalah ....",
@@ -423,7 +423,7 @@ window.soalING = [
 // ========================================
 {
     id: 52,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Hewan yang mampu menyesuaikan diri dengan lingkungan akan bertahan dari proses seleksi alam. Hewan mengalami perkembangan pada organ atau kebiasaan untuk fungsi tertentu, misalnya beberapa hewan berikut:<br>(1) Kalajengking<br>(2) Bunglon<br>(3) Cicak<br>(4) Udang<br>(5) Bulu babi laut<br>(6) Belalang<br><br>Hewan yang memiliki kesamaan dalam adaptasi ditunjukkan nomor ....",
@@ -442,7 +442,7 @@ window.soalING = [
 // ========================================
 {
     id: 53,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PGK",
     soal: "Di kota-kota besar kita bisa menemukan kedai kopi yang menggunakan wadah plastik yang estetik. Tidak hanya menghilangkan dahaga, berbagi jenis mer kopi menjadi sebutan standar hidup dan menunjukan kebersamaan masyarakat. Akan tetapi, kegiatain ini berdampak buruk karena memperbanyak jumlah sampah plastik yang sulit untuk terurai.<br><br>Oleh karena itu, kita perlu melakukan permasalahan dengan melakukan beberapa solusi. Upaya yang tepat untuk menjaga kelestarian lingkungan yang sesuai dengan ilustrasi tersebut adalah ....<br><br>Solusi<br>I. Menggunakan tumbler<br>II. Mendaulat ulang sampah plastik<br>III. Mengubur sampah plastik<br>IV. Mengolah sisa makanan menjadi pupuk<br>V. Membakar sampah plastik yang menumpuk",
@@ -461,7 +461,7 @@ window.soalING = [
 // ========================================
 {
     id: 54,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PGK",
     soal: "Azka mengamati proses daur hidup hewan mulai dari telur hingga dewasa. Azka menemukan bahwa terdapat beberapa hewan yang mengalami perubahan yang sangat berbeda bentuk dari saat masih sampai dewasa, tetapi ada juga hewan yang setelah menetas menetas dewasa tidak terlihat perbedaannya. Proses perubahan bentuk ini disebut dengan metamorfosis.<br><br>Azka menemukan 4 gambar hewan sebagai berikut:<br>(I) Kupu-kupu<br>(II) Semut<br>(III) Kecoa<br>(IV) Belalang<br><br>Analisis yang tepat dari keempat hewan tersebut adalah .... (jawaban lebih dari satu)<br><br>(1) Kupu-kupu mengalami metamorfosis sempurna karena tidak mengalami fase kepompong dan memiliki bentuk berbeda antara menetas dewasa.<br>(2) Semut merupakan hewan dengan metamorfosis sempurna karena melalui 4 tahap yaitu telur, larva, pupa dan dewasa.<br>(3) Kecoa mengalami metamorfosis tidak sempurna karena tidak mengalami fase kepompong.<br>(4) Belalang tidak mengalami metamorfosis sempurna karena setelah menetas telur berubah menjadi mirip induk tetapi dan menjadi bertambah besar.",
@@ -480,7 +480,7 @@ window.soalING = [
 // ========================================
 {
     id: 55,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Setiap hewan memiliki bagian tubuh yang memiliki fungsi tertentu. Beberapa hewan yang memiliki keunikan disajikan dalam tabel berikut.<br><br>Pasangan yang benar antara bagian tubuh hewan dengan fungsinya ditunjukkan oleh ...",
@@ -499,7 +499,7 @@ window.soalING = [
 // ========================================
 {
     id: 56,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Perhatikan gambar berikut!<br><br>Berikut ini pernyataan yang tepat mengenai otot sesuai dengan gambar di atas adalah ....",
@@ -518,7 +518,7 @@ window.soalING = [
 // ========================================
 {
     id: 57,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Perhatikan gambar berikut!<br><br>Sesuai dengan gambar di atas, mekanisme yang ditunjukkan nomor 3-6-1-2 adalah ....",
@@ -537,7 +537,7 @@ window.soalING = [
 // ========================================
 {
     id: 58,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Aini mengalami rasa sakit pada sistem pencernaan. Setelah dicek oleh dokter, ternyata pada sistem pencernaan Aini mengandung bakteri Helicobacter pylori. Gejala yang dialami Aini seperti nyeri pada bagian perut, perih, dan mual. Aini diperiksa dan oleh dokter untuk tidak mengindahkan makanan dan minuman yang dapat mengiritasi organ, seperti makanan pedas, asam, berlemak tinggi, bumbu masakan, dan berkafein seperi kopi. Penyakit yang dialami Aini menyerang organ ....",
@@ -556,7 +556,7 @@ window.soalING = [
 // ========================================
 {
     id: 59,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Perhatikan gambar berikut!<br><br>Cara perkembangbiakan tanaman dari gambar secara berurutan (a), (b), dan (c) adalah ....",
@@ -575,7 +575,7 @@ window.soalING = [
 // ========================================
 {
     id: 60,
-    kategori: "IPA",
+    kategori: "BAHASA INGGRIS",
     rentangNilai: "0-3",
     tipe: "PG",
     soal: "Bima merupakan seorang anak laki-laki tahun depan akan masuk sekolah menengah pertama. Bima mengalami berbagai perubahan dari bentuk fisik dan berkurangnya intensitas bermain serta mulai mendalami hobi tertentu. Bima menjelaskan perubahannya kepada ayahnya, Pak Bayu. Pak Bayu menjelaskan bahwa Bima mengalami pubertas. Berikut ini ciri-ciri pubertas primer yang dialami oleh Bima adalah ....",
